4abfa202c0f6dd3052864e9aac51ec4d9c4033dc
Prasanna Poonacha <ppoonacha@mast.com>
2020-06-12 12:30:35 +0530

    contacts context, contacts and contactitem components

---
+++ client/src/context/contact/ContactState.js
@@ -0,0 +1,69 @@
+import React, { useReducer } from "react";
+import uuid from "uuid";
+import ContactContext from "./contactContext";
+import contactReducer from "./contactReducer";
+import {
+  ADD_CONTACT,
+  DELETE_CONTACT,
+  SET_CURRENT,
+  CLEAR_CURRENT,
+  UPDATE_CONTACT,
+  FILTER_CONTACTS,
+  CLEAR_FILTER,
+} from "../types";
+
+const ContactState = (props) => {
+  const initialState = {
+    contacts: [
+      {
+        id: 1,
+        name: "Prasanna Poonacha",
+        email: "Prasanna.Poonacha@gmail.com",
+        phone: "9008825886",
+        type: "personal",
+      },
+      {
+        id: 2,
+        name: "Chaithra Poonacha",
+        email: "Chaithra.Poonacha@gmail.com",
+        phone: "9916808666",
+        type: "personal",
+      },
+      {
+        id: 3,
+        name: "Neel Subbaiah",
+        email: "Neel.Subbaiah@gmail.com",
+        phone: "0123456789",
+        type: "professional",
+      },
+    ],
+  };
+
+  const [state, dispatch] = useReducer(contactReducer, initialState);
+
+  //Add contact
+
+  //Delete contact
+
+  //Set current contact
+
+  //Clear current contact
+
+  //Update contact
+
+  //Filter contacts
+
+  //Clear filter
+
+  return (
+    <ContactContext.Provider
+      value={{
+        contacts: state.contacts,
+      }}
+    >
+      {props.children}
+    </ContactContext.Provider>
+  );
+};
+
+export default ContactState;
