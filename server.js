404b3451261519422fb9a6c898d20761eab62311
Prasanna Poonacha <ppoonacha@mast.com>
2020-06-14 17:45:03 +0530

    app deployment to heroku

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
-const connectDB = require('./config/db');
+const connectDB = require("./config/db");
+const path = require("path");
 
 const app = express();
 
@@ -7,16 +8,21 @@ const app = express();
 connectDB();
 
 //Init Middleware
-app.use(express.json({extended: false}));
-
-app.get("/", (req, res) => {
-  res.json({ msg: "Welcome to ContactKeeper API..." });
-});
+app.use(express.json({ extended: false }));
 
 //Define routes
 app.use("/api/users", require("./routes/users"));
 app.use("/api/contacts", require("./routes/contacts"));
 app.use("/api/auth", require("./routes/auth"));
+
+//Server static assets in production
+if (process.env.NODE_ENV !== "production") {
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
